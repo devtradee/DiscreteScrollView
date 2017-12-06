8af9d3e3c0b6262f5abdb2553f33a8f70ab6d39d
Vincent Molinie <vince@tradee.com>
2017-12-06 16:18:40 +0100

    Fixing scroll

--- library/src/main/java/com/yarolegovich/discretescrollview/DiscreteScrollLayoutManager.java
+++ library/src/main/java/com/yarolegovich/discretescrollview/DiscreteScrollLayoutManager.java
@@ -504,7 +504,7 @@ public class DiscreteScrollLayoutManager extends RecyclerView.LayoutManager {
         if (currentPosition == position) return;
         pendingScroll = -scrolled;
         Direction direction = Direction.fromDelta(position - currentPosition);
-        int distanceToScroll = (Math.abs(position - currentPosition) - 1) * scrollToChangeCurrent + scrollToChangeCurrentOffset;
+        int distanceToScroll = (Math.abs(position - currentPosition) - 1) * scrollToChangeCurrent + scrollToChangeCurrent;
         pendingScroll += direction.applyTo(distanceToScroll);
         pendingPosition = position;
         startSmoothPendingScroll();
